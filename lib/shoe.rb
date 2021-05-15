6024511f11d484446844eb78dae860c90e368b45
Shasha Yan <shashayan@gmail.com>
2021-05-15 17:02:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class shoe
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand
+
+def initialize(brand)
+  @initialize = brand
+end
+
+def cobble
+  puts "Your shoe is as good as new!"
+end
+
+
+
+end# Make your shoe class here!
